a0c69b0ad12dd1ce77d9be2bdc9ab03006a8e3ee
Natasha <nat4shail@yandex.ru>
2023-04-22 16:59:38 +0300

    e a

--- js/main.js
+++ js/main.js
@@ -29,3 +29,20 @@ $(document).ready(function () {
     $('.menu-burger__header, .menu').toggleClass('open-menu');
   });
 });
+
+
+function readMore() {
+  let dots = document.getElementById("dots");
+  let more = document.getElementById("more");
+  let btn = document.getElementById("btn");
+
+  if(dots.style.display === "none") {
+    dots.style.display = "inline";
+    btn.innerHTML = "Подробнее"
+    more.style.display = "none";
+  }  else {
+    dots.style.display ="none";
+    btn.innerHTML = "Скрыть"
+    more.style.display = "inline";
+  }
+}
