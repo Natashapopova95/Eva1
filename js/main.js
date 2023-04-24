bfa873e8f5710b9ab37fd389901585e49155cdad
Natasha <nat4shail@yandex.ru>
2023-04-24 16:56:05 +0300

    eva

--- js/main.js
+++ js/main.js
@@ -46,3 +46,32 @@ function readMore() {
     more.style.display = "inline";
   }
 }
+
+
+
+function init() {
+  let map = new ymaps.Map('map', {
+    center: [52.756979320669366, 41.47066227182767],
+    zoom: 17
+  });
+
+  let placemark = new ymaps.Placemark([52.757487916613094, 41.4702087610175], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: './images/logo.svg',
+    iconImageSize: [50, 50],
+    iconImageOfset: [0, 0]
+  });
+
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+
+  map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
